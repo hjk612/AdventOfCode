13ffc7870ba9e2023787f9bc025448cbf4905626
Hatim Kagalwala <hjk612@nyu.edu>
2018-12-05 17:14:15 -0500

    adding day 4 part 1

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -11,6 +11,7 @@ regex_pattern = re.compile('\[(?P<datetime>.*)\]\s(?P<entry>.*)')
 re_results = regex_pattern.findall(data) 
 
 def convert_string(input_):
+    
     '''
     function to convert string to datetime
     '''
@@ -20,6 +21,34 @@ def convert_string(input_):
 data_cleaned = [(convert_string(x[0]), x[1]) for x in re_results]
 data_cleaned.sort(key=lambda x: x[0])
 
+re_guard = re.compile('\#(\d*)\s')
+                      
+guard_id = None
+guard_time = {}
+start = None
+end = None
+for item in data_cleaned:
+    time = item[0].minute
+    status = item[1]
+    if 'Guard' in status:
+        guard_id = int(re_guard.findall(status)[0])
+        
+    elif 'asleep' in status:
+        start = time
+    
+    elif 'wakes' in status:
+        end = time
+        if guard_id in guard_time:
+            guard_time[guard_id].extend(list(range(start, end)))
+            
+        else:
+            guard_time[guard_id] = list(range(start, end))
+    
 
+### getting the id of the guard who slept most
+id_guard = max(guard_time, key=lambda x:len(guard_time[x]))
 
-
+### getting the most common frequency
+minute = max(set(guard_time[id_guard]), key=guard_time[id_guard].count)   
+        
+answer = id_guard * minute
