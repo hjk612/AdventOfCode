c8b5087b56a51d526c8852b34dca73ee3ac9225e
Hatim Kagalwala <hjk612@nyu.edu>
2018-12-05 10:15:26 -0500

    adding day 3 part 2

--- Day 3/day3-1.py
+++ Day 3/day3-1.py
@@ -8,24 +8,43 @@ import re
 
 os.chdir('/Users/Hatim/Desktop/AdventOfCode/Day 3/')
 
-regex_pattern = re.compile('@\s(?P<left>\d*),(?P<top>\d*):\s(?P<dim1>\d*)x(?P<dim2>\d*)')
+regex_pattern = re.compile('#(?P<id>\d*)\s@\s(?P<left>\d*),(?P<top>\d*):\s(?P<dim1>\d*)x(?P<dim2>\d*)')
 text_file = open('input.txt','r')
 data = text_file.read()
 
 re_results = regex_pattern.findall(data)
 
 cloth = {}
-
 for item in re_results:
-    left = int(item[0]) + 1
-    top = int(item[1]) + 1
-    x = left + int(item[2]) 
-    y = top + int(item[3]) 
-    for i in range(left, x ):
-        for j in range(top, y ):
+    id_ = int(item[0])
+    left = int(item[1]) + 1
+    top = int(item[2]) + 1
+    x = left + int(item[3]) 
+    y = top + int(item[4]) 
+    for i in range(left, x):
+        for j in range(top, y):
             if (i, j) in cloth:
                 cloth[(i, j)] += 1
             else:
                 cloth[(i, j)] = 1
+
+
+for item in re_results:
+    id_ = int(item[0])
+    left = int(item[1]) + 1
+    top = int(item[2]) + 1
+    x = left + int(item[3]) 
+    y = top + int(item[4])
+    flag = True
+    for i in range(left, x):
+        for j in range(top, y):
+            if cloth[(i,j)] > 1:
+                flag = False
+                continue
+        
+    if flag == True:
+        answer = id_
     
+            
+
     
