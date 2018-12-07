e758c89bf87f741ceeb49d047f6d6cf3aa414a1e
Hatim Kagalwala <hjk612@nyu.edu>
2018-12-07 10:19:09 -0500

    adding comments

--- Day 2/day2-2.py
+++ Day 2/day2-2.py
@@ -21,10 +21,12 @@ def box_id(input_file):
     data = read_file('input.txt')
     for i in range(len(data)):
         string = data[i]
+        ### the box will have many ids that are same but differ by only
+        ### character at the same location
         temp_match = [match(string, s) for s in data[i+1:] if match(string, s) > -1]
+        ### there will be many ids -- we only care about the common part
         if len(temp_match) > 0:
             index = temp_match[0]
             return string[:index]+string[index+1:]
-        
-answer = box_id('input.txt')
 
+answer = box_id('input.txt')
